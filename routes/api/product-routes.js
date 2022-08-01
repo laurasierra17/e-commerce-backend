f234a89812ed47761496c10329b58105633b27c8
laurasierra17 <laura.sierra17@gmail.com>
2022-08-01 01:52:45 -0400

    fix creation of a new tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -50,6 +50,7 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+ // FIX is it okay to have category id and name null?
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,7 +39,7 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.create({ tag_name: req.body });
+    const tagData = await Tag.create({ tag_name: req.body.tag_name });
     res.status(200).json(tagData)
   } catch (err) {
     res.status(500).json(err);
