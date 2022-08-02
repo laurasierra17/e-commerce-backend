278774c397dd228b4d3f4fa929de5b8ac2066f0f
laurasierra17 <laura.sierra17@gmail.com>
2022-08-01 22:34:18 -0400

    edit foreign keys

--- models/index.js
+++ models/index.js
@@ -15,7 +15,7 @@ Category.hasMany(Product, {
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'id', 
+  foreignKey: 'product_id', 
   through: {
     model: ProductTag,
     unique: false 
@@ -24,7 +24,7 @@ Product.belongsToMany(Tag, {
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, { 
-  foreignKey: 'id',
+  foreignKey: 'tag_id',
   through: {
     model: ProductTag,
     unique: false 
